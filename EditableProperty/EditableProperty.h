738babc34da2b480f0b596d152affcb72affe807
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2015-05-01 10:11:08 +0200

    Add Xcode project and framework

---
+++ EditableProperty/EditableProperty.h
@@ -0,0 +1,19 @@
+//
+//  EditableProperty.h
+//  EditableProperty
+//
+//  Created by Justin Spahr-Summers on 2015-05-01.
+//  Copyright (c) 2015 ReactiveCocoa. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EditableProperty.
+FOUNDATION_EXPORT double EditablePropertyVersionNumber;
+
+//! Project version string for EditableProperty.
+FOUNDATION_EXPORT const unsigned char EditablePropertyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EditableProperty/PublicHeader.h>
+
+
