f91efbec17969e6b3bb4289c7c8053f47f846d18
Neil Pankey <npankey@gmail.com>
2015-06-15 20:28:22 -0700

    Hookup the iOS target

--- EditableProperty/EditableProperty.h
+++ EditableProperty/EditableProperty.h
@@ -6,14 +6,10 @@
 //  Copyright (c) 2015 ReactiveCocoa. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for EditableProperty.
 FOUNDATION_EXPORT double EditablePropertyVersionNumber;
 
 //! Project version string for EditableProperty.
 FOUNDATION_EXPORT const unsigned char EditablePropertyVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <EditableProperty/PublicHeader.h>
-
-
