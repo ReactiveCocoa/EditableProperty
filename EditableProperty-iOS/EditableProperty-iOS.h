6eaf796641fd1d52c53ea4e44bd7440468dfd431
Neil Pankey <npankey@gmail.com>
2015-06-15 20:17:20 -0700

    Add an iOS target

---
+++ EditableProperty-iOS/EditableProperty-iOS.h
@@ -0,0 +1,19 @@
+//
+//  EditableProperty-iOS.h
+//  EditableProperty-iOS
+//
+//  Created by Neil Pankey on 6/15/15.
+//  Copyright (c) 2015 ReactiveCocoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EditableProperty-iOS.
+FOUNDATION_EXPORT double EditableProperty_iOSVersionNumber;
+
+//! Project version string for EditableProperty-iOS.
+FOUNDATION_EXPORT const unsigned char EditableProperty_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EditableProperty_iOS/PublicHeader.h>
+
+
