0c650020593790e6d145924b69813119ebde9e27
Christopher Liscio <chris@supermegaultragroovy.com>
2016-01-09 16:18:49 -0500

    Revert "Add a few helpful operators"
    This reverts commit 3da114ae8e07a243cbcefab0aa12852693b7eca8.

--- EditablePropertyTests/EditablePropertySpec.swift
+++ EditablePropertyTests/EditablePropertySpec.swift
@@ -16,7 +16,8 @@ typealias TestEditor = Editor<Int, NoError>
 class EditablePropertySpec: QuickSpec {
 	override func spec() {
 		it("should do some binding magic") {
-			let defaultValue = MutableProperty(0)
+			let defaultValue
+            = MutableProperty(0)
 
 			let property = EditableProperty<Int, NoError>(defaultValue: defaultValue, editsTakePriority: false)
 			expect(property.value).to(equal(defaultValue.value))
@@ -54,61 +55,5 @@ class EditablePropertySpec: QuickSpec {
 			tryEdit(4)
 			expect(property.value).to(equal(4))
 		}
-		
-		context("when working with a property holding edit sessions") {
-			let defaultValue = MutableProperty(0)
-			
-			let property = EditableProperty<Int, NoError>(defaultValue: defaultValue, editsTakePriority: false)
-			expect(property.value).to(equal(defaultValue.value))
-			
-			let editsProperty = MutableProperty<TestEditor.EditSession>(.never)
-			let editor = Editor(edits: editsProperty.producer) { committed, proposed in
-				return SignalProducer(value: max(committed.value, proposed))
-			}
-			
-			property <~ editor
-			
-			beforeEach {
-				defaultValue.value = 1
-				expect(property.value).to(equal(defaultValue.value))
-			}
-			
-			it("should take values coming across a signal") {
-				let (valueSignal, valueSink) = Signal<Int, NoError>.pipe()
-				
-				editsProperty <~ valueSignal
-				
-				valueSink.sendNext(2)
-				expect(property.value).to(equal(2))
-				
-				defaultValue.value = 3
-				expect(property.value).to(equal(defaultValue.value))
-				
-				valueSink.sendNext(2)
-				expect(property.value).to(equal(defaultValue.value))
-				
-				valueSink.sendNext(4)
-				expect(property.value).to(equal(4))
-			}
-			
-			it("should take values coming across a signalProducer") {
-				let (valueProducer, valueSink) = SignalProducer<Int, NoError>.buffer(0)
-				
-				editsProperty <~ valueProducer
-				
-				valueSink.sendNext(2)
-				expect(property.value).to(equal(2))
-				
-				defaultValue.value = 3
-				expect(property.value).to(equal(defaultValue.value))
-				
-				valueSink.sendNext(2)
-				expect(property.value).to(equal(defaultValue.value))
-				
-				valueSink.sendNext(4)
-				expect(property.value).to(equal(4))
-			}
-		}
-
 	}
 }
